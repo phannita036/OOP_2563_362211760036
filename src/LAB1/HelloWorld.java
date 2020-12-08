0b21b154d838a52fa06cdad3cbb19f855a3267ef
phannita <phannita_n036@rmutsvmail.com>
2020-12-08 11:20:49 +0700

    Test Push

---
+++ src/LAB1/HelloWorld.java
@@ -0,0 +1,21 @@
+package LAB1;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("HelloWorld");
+
+
+
+
+    }//main
+
+
+
+
+
+
+
+}//class
