44353318899f16e05c9c47c62dc1cc4613a5591f
LabCom-MT3 <phannita_n036@rmutsvmail.com>
2020-12-15 10:10:36 +0700

    multiply

--- src/LAB2/BasicJava.java
+++ src/LAB2/BasicJava.java
@@ -2,13 +2,15 @@ package LAB2;
 
 public class BasicJava {
 
-    public static void maid(String[] args){
+    public static void main(String[] args){
             System.out.println("Data Type in Programming");
             //Integer จำนวนเต็ม เช่น - 100 -10 -2 -1 0 1 2 3 10 100
             int number;
             number = 100; //ตัวแปรnumber เก็บค่าข้อมูลเต็ม 100
 
-            System.out.println((number));
+            System.out.println(number);
 
+            number= number *100;
+            System.out.println(number);
     }
 }
